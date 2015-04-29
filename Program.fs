75dfbaa40355b6b26d957f5922ff5f7fd39b0681
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-04-29 11:41:06 +1000

    initial commit

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open FSharp.Data
+
+(* Steps:
+   - Go to http://www.meetup.com/fsharpsydney/
+   - Go to the current meeting
+   - Go to the "Tools" dropdownlist on the top on the right side and select "Download attendee list"
+   - Open the file with excel and save it as .csv (e.g. FSharp-May.csv)
+   - Update the "filePath" literal in the current file to point to csv file instead of Sample.csv (e.g. let filePath = @"C:\Downloads\FSharp-April.csv") 
+   - Run the app *)
+
+[<Literal>]
+let filePath = @"Sample.csv"
+type Meeting = CsvProvider<filePath>
+
+[<EntryPoint>]
+let main argv = 
+
+    let membersToExclude = ["Jorge Fioranelli"; "Hadi Eskandari"; "Aaron Powell"]
+    let meeting = Meeting.Load filePath
+    let members = [for row in meeting.Rows do 
+                    if not (membersToExclude |> List.exists ((=)row.Name)) then
+                     yield row.Name]
+    let rnd = System.Random ()
+    members |> Random.getMember rnd
+    
---
+++ Random.fs
@@ -0,0 +1,32 @@
+﻿module Random
+
+open System
+open System.Threading
+open FSharp.Data
+
+
+let rec getMember (rnd: Random) members =
+
+    let rec findWinner count ls =
+
+        let printMember m = async {
+            do! Async.Sleep 50
+            printfn "%s" m }
+
+        match (ls, count) with
+        | ([], 0) when members |> Seq.length = 0 -> "No members found" 
+        | ([], _) -> members |> findWinner count 
+        | (m::ms, 0) -> m
+        | (m::ms, _) -> 
+            printMember m |> Async.RunSynchronously 
+            ms |> findWinner (count - 1) 
+
+    let membersCount = members |> Seq.length
+    let winnerIndex = membersCount * 2 + rnd.Next(membersCount)
+    let winner = members |> findWinner winnerIndex
+    printfn "-> Winner: %s" winner
+    Console.ReadLine() |> ignore
+    members |> List.filter ((<>) winner) |> getMember rnd
+
+
+
